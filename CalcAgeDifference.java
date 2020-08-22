b47145776f5221808663f4a720540d9ce5880c8a
Davi Silva <davisilvaphoto@gmail.com>
2020-08-22 09:02:22 -0300

    created CalcAgeDifference class

---
+++ CalcAgeDifference.java
@@ -0,0 +1,9 @@
+public class CalcAgeDifference {
+    public static void main(String[] args) {
+        int age1 = 34;
+        int age2 = 30;
+        int ageDifference = age1 - age2;
+
+        System.out.println(ageDifference);
+    }
+}
