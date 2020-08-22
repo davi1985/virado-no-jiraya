553c3982bea9566ab4ac30de371774fee4c500f9
Davi Silva <davisilvaphoto@gmail.com>
2020-08-22 09:38:40 -0300

    first class in intellij

---
+++ lessons/src/AgeCalc.java
@@ -0,0 +1,8 @@
+public class AgeCalc {
+    public static void main(String[] args) {
+        int age1 = 34;
+        int age2 = 30;
+        int ageDifference = age1 -age2;
+        System.out.println(ageDifference);
+    }
+}
