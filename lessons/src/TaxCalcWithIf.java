5141a210aba842a0a924462963aab89481d62696
Davi Silva <davisilvaphoto@gmail.com>
2020-09-08 07:48:23 -0300

    if and else examples

--- lessons/src/TaxCalcWithIf.java
+++ lessons/src/TaxCalcWithIf.java
@@ -1,15 +1,16 @@
 public class TaxCalcWithIf {
     public static void main(String[] args) {
-        double salary = 4300.00;
+        double salary = 4550.00;
 
         double tax;
 
-        if (salary >= 4500) {
+        if (salary > 4500) {
             tax = 0.3 * salary;
-            System.out.println(salary - tax);
         } else {
             tax = 0.15 * salary;
-            System.out.println(salary - tax);
         }
+
+        System.out.println("Tax: " + tax);
+        System.out.println("Balance: $" + (salary - tax));
     }
 }
