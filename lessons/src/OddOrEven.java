57a65c5ea189ba0d6ca1dc6c7d4876e18656c640
Davi Silva <davisilvaphoto@gmail.com>
2020-09-08 07:55:39 -0300

    odd or even example with if/else and boolean

---
+++ lessons/src/OddOrEven.java
@@ -0,0 +1,13 @@
+public class OddOrEven {
+    public static void main(String[] args) {
+        int number = 6;
+        boolean isEven = number % 2 == 0;
+
+        if (isEven)
+            System.out.println("The number is EVEN");
+        else
+            System.out.println("The number is ODD");
+
+
+    }
+}
